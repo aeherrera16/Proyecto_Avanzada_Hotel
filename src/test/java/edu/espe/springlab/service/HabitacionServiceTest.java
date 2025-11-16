f097fb141e215a80277816bc226624f4bde38650
Anahy Estefanía Herrera Quinte <117839629+aeherrera16@users.noreply.github.com>
2025-11-16 02:20:03 -0500

    test: directo

--- src/test/java/edu/espe/springlab/service/HabitacionServiceTest.java
+++ src/test/java/edu/espe/springlab/service/HabitacionServiceTest.java
@@ -1,3 +1,19 @@
 package edu.espe.springlab.service;
+import edu.espe.springlab.service.habitacion.HabitacionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
 public class HabitacionServiceTest {
+    @Autowired
+    private HabitacionService habitacionService;
+    @Test
+    void testServiceLoads() {
+        assertThat(habitacionService).isNotNull();
+    }
+    @Test
+    void testFindAll() {
+        assertThat(habitacionService.findAll()).isNotNull();
+    }
 }
