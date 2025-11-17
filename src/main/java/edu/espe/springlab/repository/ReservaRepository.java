a3bed7f98cf00aa242a02472150073fc5f8623f0
Anahy Estefanía Herrera Quinte <117839629+aeherrera16@users.noreply.github.com>
2025-11-17 00:55:32 -0500

    Ultima modificacion

--- src/main/java/edu/espe/springlab/repository/ReservaRepository.java
+++ src/main/java/edu/espe/springlab/repository/ReservaRepository.java
@@ -2,8 +2,17 @@ package edu.espe.springlab.repository;
 
 import edu.espe.springlab.domain.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    @Modifying
+    @Query("DELETE FROM Reserva r WHERE r.huesped.id = :huespedId")
+    void deleteByHuespedId(@Param("huespedId") Long huespedId);
+
+    boolean existsByHuespedId(Long huespedId);
 }
