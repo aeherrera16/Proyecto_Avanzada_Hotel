0b5f8e5de16a017fac2d5a81fbe9f1810f6c9400
Paul Gualotuna <kpgualotuna1@espe.edu.ec>
2025-12-22 11:13:03 -0500

    Ajustes exception reactive

--- src/main/java/edu/espe/springlab/exception/reactive/ReactiveResourceNotFoundException.java
+++ src/main/java/edu/espe/springlab/exception/reactive/ReactiveResourceNotFoundException.java
@@ -1,11 +1,14 @@
 package edu.espe.springlab.exception.reactive;
 
+// Excepción personalizada para manejar recursos no encontrados en un contexto reactivo
 public class ReactiveResourceNotFoundException extends RuntimeException {
-    
+
+    // Constructor que recibe un mensaje de error
     public ReactiveResourceNotFoundException(String message) {
         super(message);
     }
-    
+
+    // Constructor que recibe un mensaje de error y la causa original (excepción anidada)
     public ReactiveResourceNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
